2f047fd70069b0c2e12d678489ffa504a4928aa7
Luc <luc.schulze@ecube.de>
2022-09-16 16:14:10 +0200

    refactored CRUD operation methods

--- src/main/java/com/example/demo/web/AuthorController.java
+++ src/main/java/com/example/demo/web/AuthorController.java
@@ -1,62 +1,30 @@
 package com.example.demo.web;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @ResponseBody
 @RequestMapping(value = {"/api/author"})
 class AuthorController {
 
-    private List<Author> authorList = new ArrayList();
-
-    public String errorMessage = "The author you requested doesn't exist. Please review your parameters!";
-
-    private int getAuthorIndexInAuthorList(String authorId) {
-        for(int i = 0; i < this.authorList.size(); ++i) {
-            Author currentItem = this.authorList.get(i);
-            if (currentItem.getId().equals(authorId)) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
     @PostMapping(consumes = {"application/json"},
                 produces = {"application/json"})
     Author addAuthor(@RequestBody Author author) {
-        String authorId = UUID.randomUUID().toString();
-        author.setId(authorId);
-        this.authorList.add(author);
-        return author;
+        return DatenBank.postAuthors(author);
     }
 
     @ResponseBody
     @GetMapping(value = {"/{authorId}"},
                 produces = {"application/json"})
     Author getAuthor(@PathVariable String authorId){
-        if (this.getAuthorIndexInAuthorList(authorId) >= 0) {
-            Author author = this.authorList.get(this.getAuthorIndexInAuthorList(authorId));
-            return author;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, this.errorMessage);
-        }
+        return DatenBank.getAuthors(authorId);
     }
 
     @ResponseBody
     @DeleteMapping(value = {"/{authorId}"},
                    consumes = {"application/json"})
     void removeAuthor(@PathVariable String authorId){
-        if (this.getAuthorIndexInAuthorList(authorId) >= 0) {
-            this.authorList.remove(this.getAuthorIndexInAuthorList(authorId));
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, this.errorMessage);
-        }
+        DatenBank.deleteAuthors(authorId);
     }
 
     @ResponseBody
@@ -64,12 +32,6 @@ class AuthorController {
                 consumes = {"application/json"},
                 produces = {"application/json"})
     Author updateAuthor(@PathVariable String authorId, @RequestBody Author authorFromUser){
-        if (this.getAuthorIndexInAuthorList(authorId) >= 0) {
-            authorFromUser.setId(authorId);
-            this.authorList.set(this.getAuthorIndexInAuthorList(authorId), authorFromUser);
-            return authorFromUser;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, this.errorMessage);
-        }
+        return DatenBank.putAuthors(authorId, authorFromUser);
     }
 }
---
+++ src/main/java/com/example/demo/web/DatenBank.java
@@ -0,0 +1,58 @@
+package com.example.demo.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class DatenBank {
+
+    private static List<Author> authorList = new ArrayList();
+
+    public static String errorMessage = "The author you requested doesn't exist. Please review your parameters!";
+
+    private static int getAuthorIndexInAuthorList(String authorId) {
+        for(int i = 0; i < authorList.size(); ++i) {
+            Author currentItem = authorList.get(i);
+            if (currentItem.getId().equals(authorId)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static Author postAuthors(Author author) {
+        String authorId = UUID.randomUUID().toString();
+        author.setId(authorId);
+        authorList.add(author);
+        return author;
+    }
+
+    public static Author getAuthors(String authorId) {
+        if (getAuthorIndexInAuthorList(authorId) >= 0) {
+            Author author = authorList.get(getAuthorIndexInAuthorList(authorId));
+            return author;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
+        }
+    }
+
+    public static void deleteAuthors(String authorId) {
+        if (getAuthorIndexInAuthorList(authorId) >= 0) {
+            authorList.remove(getAuthorIndexInAuthorList(authorId));
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
+        }
+    }
+
+    public static Author putAuthors(String authorId, Author authorFromUser) {
+        if (getAuthorIndexInAuthorList(authorId) >= 0) {
+            authorFromUser.setId(authorId);
+            authorList.set(getAuthorIndexInAuthorList(authorId), authorFromUser);
+            return authorFromUser;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
+        }
+    }
+}

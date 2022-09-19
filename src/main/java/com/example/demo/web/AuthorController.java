93c76d67292d7e8216edce5efcf438658ff0b789
Luc <luc.schulze@ecube.de>
2022-09-19 10:44:06 +0200

    refactored CRUD operation methods and renaming of classes

--- src/main/java/com/example/demo/web/AuthorController.java
+++ src/main/java/com/example/demo/web/AuthorController.java
@@ -7,24 +7,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = {"/api/author"})
 class AuthorController {
 
+    AuthorRepository repository = new AuthorRepository();
+
     @PostMapping(consumes = {"application/json"},
                 produces = {"application/json"})
     Authors addAuthor(@RequestBody Authors author) {
-        return DatenBank.postAuthors(author);
+        return repository.postAuthors(author);
     }
 
     @ResponseBody
     @GetMapping(value = {"/{authorId}"},
                 produces = {"application/json"})
     Authors getAuthor(@PathVariable String authorId){
-        return DatenBank.getAuthors(authorId);
+        return repository.getAuthors(authorId);
     }
 
     @ResponseBody
     @DeleteMapping(value = {"/{authorId}"},
                    consumes = {"application/json"})
     void removeAuthor(@PathVariable String authorId){
-        DatenBank.deleteAuthors(authorId);
+        repository.deleteAuthors(authorId);
     }
 
     @ResponseBody
@@ -32,6 +34,6 @@ class AuthorController {
                 consumes = {"application/json"},
                 produces = {"application/json"})
     Authors updateAuthor(@PathVariable String authorId, @RequestBody Authors authorFromUser){
-        return DatenBank.putAuthors(authorId, authorFromUser);
+        return repository.putAuthors(authorId, authorFromUser);
     }
 }
--- src/main/java/com/example/demo/web/AuthorRepository.java
+++ src/main/java/com/example/demo/web/AuthorRepository.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class DatenBank {
+public class AuthorRepository {
 
-    private static List<Authors> authorList = new ArrayList();
+    private List<Authors> authorList = new ArrayList();
 
-    public static String errorMessage = "The author you requested doesn't exist. Please review your parameters!";
+    private String errorMessage = "The author you requested doesn't exist. Please review your parameters!";
 
-    private static int getAuthorIndexInAuthorList(String authorId) {
+    private int getAuthorIndexInAuthorList(String authorId) {
         for(int i = 0; i < authorList.size(); ++i) {
             Authors currentItem = authorList.get(i);
             if (currentItem.getId().equals(authorId)) {
@@ -22,14 +22,14 @@ public class DatenBank {
         return -1;
     }
 
-    public static Authors postAuthors(Authors author) {
+    public Authors postAuthors(Authors author) {
         String authorId = UUID.randomUUID().toString();
         author.setId(authorId);
         authorList.add(author);
         return author;
     }
 
-    public static Authors getAuthors(String authorId) {
+    public Authors getAuthors(String authorId) {
         if (getAuthorIndexInAuthorList(authorId) >= 0) {
             Authors author = authorList.get(getAuthorIndexInAuthorList(authorId));
             return author;
@@ -38,7 +38,7 @@ public class DatenBank {
         }
     }
 
-    public static void deleteAuthors(String authorId) {
+    public void deleteAuthors(String authorId) {
         if (getAuthorIndexInAuthorList(authorId) >= 0) {
             authorList.remove(getAuthorIndexInAuthorList(authorId));
         } else {
@@ -46,7 +46,7 @@ public class DatenBank {
         }
     }
 
-    public static Authors putAuthors(String authorId, Authors authorFromUser) {
+    public Authors putAuthors(String authorId, Authors authorFromUser) {
         if (getAuthorIndexInAuthorList(authorId) >= 0) {
             authorFromUser.setId(authorId);
             authorList.set(getAuthorIndexInAuthorList(authorId), authorFromUser);

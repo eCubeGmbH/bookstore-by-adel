b9937aca1e64fd2e59d681ce93193cb40f21a288
Luc <luc.schulze@ecube.de>
2022-09-19 15:30:22 +0200

    interface updated

--- src/main/java/com/example/demo/web/AuthorRepositoryMapImpl.java
+++ src/main/java/com/example/demo/web/AuthorRepositoryMapImpl.java
@@ -4,59 +4,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+
+import java.util.*;
 
 public class AuthorRepositoryMapImpl implements AuthorRepository {
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthorRepositoryMapImpl.class);
-
-    private List<Authors> authorList = new ArrayList();
+    private Map<String, Authors> authorMap = new HashMap<>();
 
     private String errorMessage = "The author you requested doesn't exist. Please review your parameters!";
 
-    private int getAuthorIndexInAuthorList(String authorId) {
-        for(int i = 0; i < authorList.size(); ++i) {
-            Authors currentItem = authorList.get(i);
-            if (currentItem.getId().equals(authorId)) {
-                return i;
-            }
+    private Object errorChecking(String authorId) {
+        if(authorMap.get(authorId) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
         }
-        return -1;
+        return new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
     }
 
     public Authors postAuthors(Authors author) {
         String authorId = UUID.randomUUID().toString();
         author.setId(authorId);
-        authorList.add(author);
+        authorMap.put(authorId, author);
         return author;
     }
 
     public Authors getAuthors(String authorId) {
         LOGGER.info("coming from map");
-        if (getAuthorIndexInAuthorList(authorId) >= 0) {
-            Authors author = authorList.get(getAuthorIndexInAuthorList(authorId));
-            return author;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
-        }
+        errorChecking(authorId);
+        Authors getAuthor = authorMap.get(authorId);
+        return getAuthor;
     }
 
     public void deleteAuthors(String authorId) {
-        if (getAuthorIndexInAuthorList(authorId) >= 0) {
-            authorList.remove(getAuthorIndexInAuthorList(authorId));
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
-        }
+        errorChecking(authorId);
+        authorMap.remove(authorId);
     }
 
     public Authors putAuthors(String authorId, Authors authorFromUser) {
-        if (getAuthorIndexInAuthorList(authorId) >= 0) {
-            authorFromUser.setId(authorId);
-            authorList.set(getAuthorIndexInAuthorList(authorId), authorFromUser);
-            return authorFromUser;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, errorMessage);
-        }
+        errorChecking(authorId);
+        authorFromUser.setId(authorId);
+        authorMap.replace(authorId, authorFromUser);
+        return authorFromUser;
     }
 }

a0e6a43fa55c955e1cbbf8ccc03078e7beb04b0a
Adel Kharbout <Adel.Kharbout@ecube.de>
2024-02-13 12:01:08 +0100

    important files

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyMainComponent from '../src/MyMainComponent';
+
+const App = () => {
+    return (
+        <div>
+            <MyMainComponent/>
+        </div>
+    );
+};
+
+export default App;

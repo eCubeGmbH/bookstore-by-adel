fa8cd5481023385eb4caf18e71675e1ec1eae979
Sven Reigl <Sven.Reigl@ecube.de>
2024-02-22 17:54:20 +0100

    - update docker / docker-compose for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 3000,
   },
+  preview:{
+    port: 3000,
+  }
 })
